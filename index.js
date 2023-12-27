1fad27f4ec642d5c2089f29d14705167540b50c8
myriam chahlafi <myriam.chah@gmail.com>
2023-12-28 00:37:56 +0800

    Get a trailer for Game page

--- index.js
+++ index.js
@@ -32,10 +32,17 @@ app.get("/games/:gameSlug", async (req, res) => {
       `https://api.rawg.io/api/games/${req.params.gameSlug}/game-series?key=${process.env.RAWG_KEY}`
     );
 
+    const trailers = await axios.get(
+      `https://api.rawg.io/api/games/${req.params.gameSlug}/movies?key=${process.env.RAWG_KEY}`
+    );
+
+    const trailer = trailers.data.results[0];
+
     res.json({
       game: response.data,
       screenshots: screenshots.data.results,
       others: others.data.results,
+      trailer: trailer,
     });
   } catch (error) {
     res.status(400).json({ message: error.message });
